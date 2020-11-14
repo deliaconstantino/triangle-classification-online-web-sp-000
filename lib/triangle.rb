34198791d43b2ca1ea1921c71073b3f246aba51a
Delia Constantino <dconstan92@gmail.com>
2020-11-14 00:05:14 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,17 +4,16 @@ class Triangle
   end
 
   def kind
-    if @all_sides.any? { |num| num <= 0 }
-      raise TriangleError
-    elsif @all_sides.any? { |num| num >= @all_sides.sum - num }
-      raise TriangleError
-    elsif @all_sides[0] == @all_sides[1] && @all_sides[1] == @all_sides[2]
-      return :equilateral
-    elsif @all_sides[0] != @all_sides[1] && @all_sides[1] != @all_sides[2] && @all_sides[0] != @all_sides[2]
-      return :scalene
-    else
-      return :isosceles
-    end
+    raise TriangleError if is_any_side_invalid?
+    raise TriangleError if @all_sides.any? { |num| num >= @all_sides.sum - num }
+    return :equilateral if @all_sides[0] == @all_sides[1] && @all_sides[1] == @all_sides[2]
+    return :scalene if @all_sides[0] != @all_sides[1] && @all_sides[1] != @all_sides[2] && @all_sides[0] != @all_sides[2]
+
+    return :isosceles
+  end
+
+  def is_any_side_invalid?
+    @all_sides.any? { |num| num <= 0 }
   end
 
   class TriangleError < StandardError
