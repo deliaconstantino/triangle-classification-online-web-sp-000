35abe2e3732a89153b8d15e08046961ad745c99a
Delia Constantino <dconstan92@gmail.com>
2020-11-14 00:03:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,5 @@
 class Triangle
-  attr_accessor :all_sides
-
   def initialize(side_a, side_b, side_c)
-    # @side_a = side_a
-    # @side_b = side_b
-    # @side_c = side_c
     @all_sides = [side_a, side_b, side_c]
   end
 
