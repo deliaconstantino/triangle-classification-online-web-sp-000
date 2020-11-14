321f4697f64eb17233cc9a03fed5bbec21ddda98
Delia Constantino <dconstan92@gmail.com>
2020-11-14 00:02:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,12 +2,11 @@ class Triangle
   attr_accessor :side_a, :side_b, :side_c, :all_sides
 
   def initialize(side_a, side_b, side_c)
-    @side_a = side_a
-    @side_b = side_b
-    @side_c = side_c
-    @all_sides = [@side_a, @side_b, @side_c]
+    # @side_a = side_a
+    # @side_b = side_b
+    # @side_c = side_c
+    @all_sides = [side_a, side_b, side_c]
   end
-
 
   def kind
     if @all_sides.any? {|num| num <= 0}
